1d59cac8f83300938d401e000bf9a45e8bf75b83
heCarlo <carlosgfkp@outlook.com>
2024-06-06 20:50:38 -0300

    fix: fixing cors

--- backend-project/src/main.ts
+++ backend-project/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as cors from 'cors'; // Importe o pacote cors
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configura o CORS para permitir solicitações apenas do front-end em localhost:3000
+  app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true, // Permitir que o navegador envie cookies
+  }));
 
   const options = new DocumentBuilder()
     .setTitle('Pegasus Shop')
@@ -13,6 +20,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
