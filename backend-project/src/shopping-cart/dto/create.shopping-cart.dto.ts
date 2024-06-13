8b427f879c1c4903360f7bb8fced8fae22e52e01
heCarlo <carlosgfkp@outlook.com>
2024-06-13 20:41:44 -0300

    refator(shopping-cart): updated the swaggert documentation

--- backend-project/src/shopping-cart/dto/create.shopping-cart.dto.ts
+++ backend-project/src/shopping-cart/dto/create.shopping-cart.dto.ts
@@ -1,12 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { ShoppingCartItemDto } from './shopping-cart.dto';
 
 export class CreateShoppingCartDto {
-  @ApiProperty()
+  @ApiProperty({ example: '66552bfc2ded290e7e379d17' })
   userId: string;
 
-  @ApiProperty()
-  items: {
-    productId: string;
-    quantity: number;
-  }[];
+  @ApiProperty({ type: [ShoppingCartItemDto], example: [
+    {
+      productId: '666b3bf0bff0b1cc6fdea87d',
+      quantity: 1
+    },
+    {
+      productId: '666b3bf0bff0b1cc6fdea87e',
+      quantity: 2
+    }
+  ] })
+  items: ShoppingCartItemDto[];
 }
