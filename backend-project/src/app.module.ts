e5c8ca635235b18944da69b72862746005299313
heCarlo <carlosgfkp@outlook.com>
2024-06-13 15:46:12 -0300

    refactor(catalog): adapted the modules to the new format at catalog app

--- backend-project/src/app.module.ts
+++ backend-project/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CatalogService } from './catalog/catalog.service';
+import { CatalogService } from './catalog/services/catalog.service';
 import { CatalogModule } from './catalog/catalog.module';
 import { ShoppingCartService } from './shopping-cart/shopping-cart.service';
 import { ShoppingCartModule } from './shopping-cart/shopping-cart.module';
@@ -10,7 +10,7 @@ import { UserController } from './users/user.controller';
 import { UserService } from './users/user.service';
 import { UserModule } from './users/user.module';
 import { TransactionController } from './transaction/transaction.controller';
-import { CatalogController } from './catalog/catalog.controller';
+import { CatalogController } from './catalog/controllers/catalog.controller';
 
 @Module({
   imports: [
@@ -19,7 +19,7 @@ import { CatalogController } from './catalog/catalog.controller';
     TransactionModule,
     UserModule,
     MongooseModule.forRoot('mongodb://localhost:27017/pegasus-shop'), 
-    ],
+  ],
   controllers: [
     UserController,
     TransactionController,
--- backend-project/src/catalog/catalog.module.ts
+++ backend-project/src/catalog/catalog.module.ts
@@ -1,13 +1,38 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CatalogController } from './catalog.controller';
-import { CatalogService } from './catalog.service';
+import { CatalogController } from './controllers/catalog.controller';
+import { CatalogService } from './services/catalog.service';
 import { Catalog, CatalogSchema } from './schemas/catalog.schema';
+import { CatalogRepository } from './repositories/catalog.repository';
+import { CreateCatalogUseCase } from './use-cases/create-catalog.usecase';
+import { GetAllCatalogsUseCase } from './use-cases/get-all-catalogs.usecase';
+import { GetCatalogByIdUseCase } from './use-cases/get-catalog-by-id.usecase';
+import { UpdateCatalogUseCase } from './use-cases/update-catalog.usecase';
+import { DeleteCatalogUseCase } from './use-cases/delete-catalog.usecase';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Catalog.name, schema: CatalogSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Catalog.name, schema: CatalogSchema }])
+  ],
   controllers: [CatalogController],
-  providers: [CatalogService],
-  exports: [CatalogService, MongooseModule.forFeature([{ name: Catalog.name, schema: CatalogSchema }])],
+  providers: [
+    CatalogRepository,
+    CatalogService,
+    CreateCatalogUseCase,
+    GetAllCatalogsUseCase,
+    GetCatalogByIdUseCase,
+    UpdateCatalogUseCase,
+    DeleteCatalogUseCase
+  ],
+  exports: [
+    CatalogRepository,
+    CatalogService,
+    CreateCatalogUseCase,
+    GetAllCatalogsUseCase,
+    GetCatalogByIdUseCase,
+    UpdateCatalogUseCase,
+    DeleteCatalogUseCase,
+    MongooseModule.forFeature([{ name: Catalog.name, schema: CatalogSchema }])
+  ],
 })
 export class CatalogModule {}
