6542318dc63e6d4256980fd3916055671f0e8562
heCarlo <carlosgfkp@outlook.com>
2024-06-14 19:53:32 -0300

    refactor(catalog): removed old use cases and reformulate it to a new format in find /manage-shopping-cart.usecase

--- backend-project/src/shopping-cart/use-cases/manage-shopping-cart.usecase.ts
+++ backend-project/src/shopping-cart/use-cases/manage-shopping-cart.usecase.ts
@@ -1,13 +1,25 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { ShoppingCart } from '../schemas/shopping-cart.schema';
 import { ShoppingCartRepository } from '../repositories/shopping-cart.repository';
+import { CreateShoppingCartDto } from '../dto/create.shopping-cart.dto';
 import { UpdateShoppingCartDto } from '../dto/update.shopping-cart.dto';
 
 @Injectable()
-export class UpdateShoppingCartUseCase {
+export class ManageShoppingCartUseCase {
   constructor(private readonly shoppingCartRepository: ShoppingCartRepository) {}
 
-  async execute(id: string, updateShoppingCartDto: UpdateShoppingCartDto): Promise<ShoppingCart> {
+  async create(createShoppingCartDto: CreateShoppingCartDto): Promise<ShoppingCart> {
+    return this.shoppingCartRepository.create(createShoppingCartDto);
+  }
+
+  async delete(id: string): Promise<void> {
+    const result = await this.shoppingCartRepository.delete(id);
+    if (result === null) {
+      throw new NotFoundException('Shopping cart not found.');
+    }
+  }
+
+  async update(id: string, updateShoppingCartDto: UpdateShoppingCartDto): Promise<ShoppingCart> {
     const updatedCart = await this.shoppingCartRepository.update(id, updateShoppingCartDto);
     if (!updatedCart) {
       throw new NotFoundException('Shopping cart not found.');
