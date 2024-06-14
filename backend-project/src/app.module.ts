29dec45960e48ac4fa940e8568edc84320c018a0
heCarlo <carlosgfkp@outlook.com>
2024-06-14 20:02:52 -0300

    refactor(app): remove the unnecessary dependencies

--- backend-project/src/app.module.ts
+++ backend-project/src/app.module.ts
@@ -1,15 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CatalogService } from './catalog/services/catalog.service';
 import { CatalogModule } from './catalog/catalog.module';
-import { ShoppingCartService } from './shopping-cart/services/shopping-cart.service';
 import { ShoppingCartModule } from './shopping-cart/shopping-cart.module';
-import { TransactionService } from './transaction/transaction.service';
 import { TransactionModule } from './transaction/transaction.module';
-import { UserController } from './users/users.controller';
-import { UsersService } from './users/shared/users.service';
-import { TransactionController } from './transaction/transaction.controller';
-import { CatalogController } from './catalog/controllers/catalog.controller';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
@@ -22,11 +15,7 @@ import { AuthModule } from './auth/auth.module';
     AuthModule,
     MongooseModule.forRoot('mongodb://localhost:27017/pegasus-shop'), 
   ],
-  controllers: [
-
-  ],
-  providers: [
-
-  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
