19d2b7396504a5f591d9d164e0ddda990c103a13
heCarlo <carlosgfkp@outlook.com>
2024-06-14 19:57:29 -0300

    refactor(shopping-cart): adapted the module to receive the new dependencies

--- backend-project/src/shopping-cart/shopping-cart.module.ts
+++ backend-project/src/shopping-cart/shopping-cart.module.ts
@@ -5,36 +5,28 @@ import { ShoppingCartController } from './controllers/shopping-cart.controller';
 import { ShoppingCartService } from './services/shopping-cart.service';
 import { ShoppingCart, ShoppingCartSchema } from './schemas/shopping-cart.schema';
 import { ShoppingCartRepository } from './repositories/shopping-cart.repository';
-import { CatalogModule } from '../catalog/catalog.module'; // Importe o módulo do catálogo
-import { CreateShoppingCartUseCase } from './use-cases/create-shopping-cart.usecase';
-import { GetAllShoppingCartsUseCase } from './use-cases/get-all-shopping-carts.usecase';
-import { GetShoppingCartByIdUseCase } from './use-cases/get-shopping-cart-by-id.usecase';
-import { UpdateShoppingCartUseCase } from './use-cases/update-shopping-cart.usecase';
-import { DeleteShoppingCartUseCase } from './use-cases/delete-shopping-cart.usecase';
+import { CatalogModule } from '../catalog/catalog.module';
+import { FindShoppingCartUseCase } from './use-cases/find-shopping-cart.usecase';
+import { ManageShoppingCartUseCase } from './use-cases/manage-shopping-cart.usecase';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: ShoppingCart.name, schema: ShoppingCartSchema }]),
-    CatalogModule, // Adicione o módulo do catálogo aqui
+    CatalogModule,
   ],
   controllers: [ShoppingCartController],
   providers: [
     ShoppingCartService,
     ShoppingCartRepository,
-    CreateShoppingCartUseCase,
-    GetAllShoppingCartsUseCase,
-    GetShoppingCartByIdUseCase,
-    UpdateShoppingCartUseCase,
-    DeleteShoppingCartUseCase,
+    FindShoppingCartUseCase,
+    ManageShoppingCartUseCase
+
   ],
   exports: [
     ShoppingCartService,
     ShoppingCartRepository,
-    CreateShoppingCartUseCase,
-    GetAllShoppingCartsUseCase,
-    GetShoppingCartByIdUseCase,
-    UpdateShoppingCartUseCase,
-    DeleteShoppingCartUseCase,
+    FindShoppingCartUseCase,
+    ManageShoppingCartUseCase
   ],
 })
 export class ShoppingCartModule {}
