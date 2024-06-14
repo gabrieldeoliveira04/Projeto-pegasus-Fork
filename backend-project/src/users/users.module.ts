7e0c9c00e5ab5f51b8c83570e7e276be103e84eb
heCarlo <carlosgfkp@outlook.com>
2024-06-14 19:49:50 -0300

    refactor(user): module adapted to import the new dependencies

--- backend-project/src/users/users.module.ts
+++ backend-project/src/users/users.module.ts
@@ -1,15 +1,28 @@
+import { UsersService } from './services/users.service';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserController } from './users.controller';
-import { UsersService } from './shared/users.service';
+import { UserController } from './controllers/users.controller';
 import { UserSchema } from './schemas/user.schema';
+import { FindUserUseCase } from './use-cases/find-user.usecase';
+import { ManageUserUseCase } from './use-cases/manage-user-usecase';
+import { UserRepository } from './repositories/user.repository';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
   ],
   controllers: [UserController],
-  providers: [UsersService],
-  exports: [UsersService],
+  providers: [
+    UsersService,
+    UserRepository,
+    FindUserUseCase,
+    ManageUserUseCase
+  ],
+  exports: [
+    UsersService,
+    UserRepository,
+    FindUserUseCase,
+    ManageUserUseCase
+  ],
 })
 export class UsersModule {}
