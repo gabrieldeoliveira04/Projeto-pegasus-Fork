552916d7b75a90e3760969400b25cf8912508f36
Gabriel Oliveira <77246516+gabrieldeoliveira04@users.noreply.github.com>
2025-05-13 13:00:25 +0000

    feat: Início de tela cadastro

---
+++ frontend-project/src/app/cadastro/tela-cadastro/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import TextComponent from './components/TextComponent'
+import Fundo_login from '../public/Fundo_login.png';
+
+function App() {
+
+  return (
+    <>
+
+        <TextComponent/>
+
+    </>
+  )
+}
+
+export default App
